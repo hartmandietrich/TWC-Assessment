6a8a2846f42900f86c902132fd99b9278fa05d84
hartmandietrich <hartmandietrich@gmail.com>
2024-11-17 17:09:32 -0500

    Add entities to DbContext and apply migrations on startup

--- TroyWebConsultingApplication/LibraryDbContext.cs
+++ TroyWebConsultingApplication/LibraryDbContext.cs
@@ -1,8 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using TroyWebConsultingApplication.Entities;
 
 namespace TroyWebConsultingApplication;
 
-public class LibraryDbContext : DbContext
+public class LibraryDbContext(DbContextOptions<LibraryDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Librarian> Librarians { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
 }
--- TroyWebConsultingApplication/Program.cs
+++ TroyWebConsultingApplication/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TroyWebConsultingApplication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LibraryDbContext>(options => options.UseSqlServer("data source=DESKTOP-S6KJK1Q\\SQLEXPRESS;initial catalog=master;trusted_connection=true;TrustServerCertificate=True"));
 
 var app = builder.Build();
 
@@ -14,6 +18,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
