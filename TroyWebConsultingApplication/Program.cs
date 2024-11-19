09b2ba1450e19336e201deffe9532f5871ba49c2
hartmandietrich <hartmandietrich@gmail.com>
2024-11-18 19:04:50 -0500

    Add initial seeding of 3 books

--- TroyWebConsultingApplication/Program.cs
+++ TroyWebConsultingApplication/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TroyWebConsultingApplication;
+using TroyWebConsultingApplication.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<LibraryDbContext>(options => options.UseSqlServer("data source=DESKTOP-S6KJK1Q\\SQLEXPRESS;initial catalog=master;trusted_connection=true;TrustServerCertificate=True"));
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+{
+    options.UseSqlServer(
+        "data source=DESKTOP-S6KJK1Q\\SQLEXPRESS;initial catalog=master;trusted_connection=true;TrustServerCertificate=True").
+    UseSeeding((context, _) =>
+    {
+        Book[] initialBooks = [new Book
+            {
+                Title = "The Shining"
+            },
+            new Book
+            {
+                Title = "Jaws"
+            },
+            new Book
+            {
+                Title = "Wild Seed"
+            }
+        ];
+    
+        if (!context.Set<Book>().Any())
+        {
+            context.Set<Book>().AddRange(initialBooks);
+            context.SaveChanges();
+        }
+    } );
+});
 
 var app = builder.Build();
 
