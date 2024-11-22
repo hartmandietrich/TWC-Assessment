ae838ecaaa994d235f08c8ef5a926992a5c5e9c2
hartmandietrich <hartmandietrich@gmail.com>
2024-11-22 15:59:28 -0500

    Add login form

--- troy_web_consulting_ui/src/Views/LoginView.vue
+++ troy_web_consulting_ui/src/Views/LoginView.vue
@@ -1,14 +1,27 @@
 <template>
-<v-row>
-  <v-col cols="12">
-    TESTING
-    <v-text-field></v-text-field>
-    <v-text-field></v-text-field>
-  </v-col>
-</v-row>
+  <v-row justify="center">
+    <v-col cols="12">
+      <v-tabs v-model="tab">
+        <v-tab value="login">Log In</v-tab>
+        <v-tab value="register">Register</v-tab>
+      </v-tabs>
+    </v-col>
+    <v-col cols="12" align-self="center">
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="login">
+          <LoginForm />
+        </v-tabs-window-item>
+        <v-tabs-window-item value="register">
+          <LoginForm />
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
-import { VTextField } from "vuetify/components";
+import LoginForm from "@/components/Forms/LoginForm.vue";
+import {ref} from "vue";
 
+var tab = ref();
 </script>
---
+++ troy_web_consulting_ui/src/components/Forms/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-form v-model="valid">
+      <v-text-field v-model="username" label="Username" :rules="usernameRules" required></v-text-field>
+      <v-text-field v-model="password" label="Password" :rules="passwordRules" required></v-text-field>
+    </v-form>
+    <v-card-actions>
+      <v-btn :disabled="!valid">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+var valid = ref(false);
+var username = ref();
+var password = ref();
+
+var usernameRules = [value => {
+  if (value) return true
+  return 'Username is required.'
+}]
+var passwordRules = [value => {
+  if (value) return true
+  return 'Password is required.'
+}]
+</script>
