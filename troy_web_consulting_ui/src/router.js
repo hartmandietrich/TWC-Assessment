d9f12096126b9b3a37dd4b17e805ebf69f340906
hartmandietrich <hartmandietrich@gmail.com>
2024-11-22 20:50:22 -0500

    delete unused files

--- troy_web_consulting_ui/src/router.js
+++ troy_web_consulting_ui/src/router.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import LoginView from "@/Views/LoginView.vue";
 
 export default createRouter({
